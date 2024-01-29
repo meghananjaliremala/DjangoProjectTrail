799af0db08cf921e4b2fbd8935bcceee1c0a9116
meghananjaliremala01~ <remalameghananjali@gmail.com>
2024-01-29 13:44:32 -0500

    first commit

---
+++ notes/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from typing import List
+from .forms import NotesForm
+from .models import Notes
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user 
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})
